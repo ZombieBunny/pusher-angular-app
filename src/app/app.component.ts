e202f2b740ea04423cd70cdd2cf90701bf32a200
Lee <leevashan.reddi@aax.co.za>
2019-08-17 18:15:47 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Pusher from 'pusher-js';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+
+  public pusherClient;
+  public channel;
+  public message = '';
+  public messages = [];
+
   title = 'chat-app-angular';
+
+  constructor(private http: HttpClient) {
+
+    this.pusherClient = new Pusher('621ee746824ccbe9eaf8', { cluster: 'ap2' });
+    this.channel = this.pusherClient.subscribe('channel');
+
+    this.channel.bind('event', (data) => {
+      console.log(data);
+      this.messages.push(data.message);
+    });
+  }
+
+  ngOnInit() {
+    // do something when component loads
+  }
+
+
+  public sendMessage(): void {
+    console.log('send');
+    this.http.post('http://localhost:3000/message', { message: this.message }).subscribe(res => {
+      console.log(res);
+    });
+    this.message = '';
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatInputModule, MatButtonModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
